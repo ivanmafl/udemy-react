f60efe5aabb2a68e1f4b3f6023f98ab2768e2ae3
Ivan <ivanbarta@gmail.com>
2021-12-04 15:37:05 +0100

    hledat

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -19,7 +19,7 @@ const SearchForm = () => {
     return <section className="section search">
         <form className="search-form" onSubmit={handleSubmit}>
             <div className="form-control">
-                <label htmlFor="name">search your favorite cocktail</label>
+                <label htmlFor="name">hledat nápoj</label>
                 <input type="text" id="name" ref={searchValue} onChange={searchCocktail}/>
             </div>
         </form>
